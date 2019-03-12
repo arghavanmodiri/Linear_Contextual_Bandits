3dec99f80d13273eff3463ff6eb5dbc3e1881d46
Hammad Shaikh <hammy.shaikh@mail.utoronto.ca>
2019-03-12 18:57:47 -0400

    Small crossover medium subgroup effect DGP

--- true_hypo_models.py
+++ true_hypo_models.py
@@ -12,7 +12,7 @@ def read_true_model(true_model_params_file='True_Model_Coefficients.csv'):
         ndarray: containing the coefficients of the true model
     """
     #noise = np.array([["noise_mean", 0],["noise_std", 5]], dtype=object)
-    noise = {"noise_mean": 0, "noise_std": 1.0}
+    noise = {"noise_mean": 0, "noise_std": 0.976}
     '''true_coeff = {"bias": 40,
                     "ch2": 10,
                     "ch3": 20, 
@@ -23,9 +23,10 @@ def read_true_model(true_model_params_file='True_Model_Coefficients.csv'):
                     "republic*matching": -5}
     context_vars = np.array(['republic'])
     experiment_vars = np.array(['ch2','ch3', 'matching'])'''
-    true_coeff = {"intercept": 0,
-                    "d1": 0.1,
-                    "d1*x1": -0.8}
+    true_coeff = {"intercept": -0.025,
+                    "d1": 0.25,
+                    "d1*x1": -0.4}
+    #true_coeff = {"intercept": 0, "d1":0.50}
     context_vars = np.array(['x1'])
     experiment_vars = np.array(['d1'])
 
@@ -65,7 +66,7 @@ def read_hypo_model(hypo_model_params_file='Hypo_Model_Design.csv'):
         list: containing the available parameters in hypothesized model
     """
     #hypo_model_params = ['bias', 'ch2','ch3', 'matching', 'republic']
-    hypo_model_params = ['intercept','d1', 'd1*x1']
+    hypo_model_params = ['intercept','d1','d1*x1']
     '''hypo_model_params =["bias",
                         "ch2",
                         "ch3", 
